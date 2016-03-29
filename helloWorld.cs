762de3a8cb5f96f40b46094ec7cef9bb60accf39
melvincheng <melvin-cheng@hotmail.com>
2016-03-29 19:53:10 -0400

    Adding test program

---
+++ helloWorld.cs
@@ -0,0 +1,10 @@
+// using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Hello, world! (no newline)");
+        Console.WriteLine("Hello, world! (newline)");
+    }
+}
