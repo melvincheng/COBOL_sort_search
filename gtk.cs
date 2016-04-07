7ed2976b148fac3db0974634202090f254b2afca
melvincheng <melvin-cheng@hotmail.com>
2016-04-07 10:24:17 -0400

    Added buttons

--- gtk.cs
+++ gtk.cs
@@ -1,16 +1,30 @@
 using System;
 using Gtk;
 
-public class GtkHelloWorld{
+class GtkHelloWorld : Window{
+	public GtkHelloWorld() : base("Everything")
+	{
+		SetDefaultSize(250, 200);
+		SetPosition(WindowPosition.Center);
+
+		Fixed fix = new Fixed();
+		Label myLabel = new Label();
+		Button button1 = new Button("Press me");
+		Button button2 = new Button("Can't press me");
+		button2.Sensitive = false;
+
+		fix.Put(button1, 100, 100);
+		fix.Put(button2, 100, 150);
+		fix.Put(myLabel, 100,10);
+
+		myLabel.Text = "Hello World";
+		Add(fix);
+		ShowAll();
+	}
+
 	public static void Main(){
 		Application.Init();
-		Window myWin = new Window("GTK# Application");
-		myWin.Resize(200,200);
-		Label myLabel = new Label();
-		myLabel.Text = "Hello World";
-		myWin.Add(myLabel);
-		myWin.ShowAll();
+		new GtkHelloWorld();
 		Application.Run();
-		// Console.WriteLine("HelloWorld");
 	}
 }
